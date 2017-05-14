0db046672a264623447de28f394ce166c1548841
sanframar <raznnok@gmail.com>
2017-05-14 13:22:23 +0200

    Registro de customer.

--- src/main/java/controllers/CustomerController.java
+++ src/main/java/controllers/CustomerController.java
@@ -10,13 +10,27 @@
 
 package controllers;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import services.CustomerService;
+import domain.Customer;
+import forms.CreateCustomerForm;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController extends AbstractController {
+
+	// Service ---------------------------------------------------------------
+	@Autowired
+	private CustomerService	customerService;
+
 
 	// Constructors -----------------------------------------------------------
 
@@ -24,25 +38,59 @@ public class CustomerController extends AbstractController {
 		super();
 	}
 
-	// Action-1 ---------------------------------------------------------------		
+	// Creation ---------------------------------------------------------------		
 
-	@RequestMapping("/action-1")
-	public ModelAndView action1() {
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
+	public ModelAndView create() {
 		ModelAndView result;
+		CreateCustomerForm createCustomerForm;
 
-		result = new ModelAndView("customer/action-1");
+		createCustomerForm = new CreateCustomerForm();
+		result = this.createEditModelAndView(createCustomerForm);
 
 		return result;
 	}
 
-	// Action-2 ---------------------------------------------------------------		
+	@RequestMapping(value = "/create", method = RequestMethod.POST, params = "save")
+	public ModelAndView saveCreate(@Valid final CreateCustomerForm createCustomerForm, final BindingResult binding) {
 
-	@RequestMapping("/action-2")
-	public ModelAndView action2() {
+		ModelAndView result;
+		Customer customer;
+
+		if (binding.hasErrors())
+			result = this.createEditModelAndView(createCustomerForm);
+		else
+			try {
+				customer = this.customerService.reconstructProfile(createCustomerForm, "create");
+				this.customerService.saveRegister(customer);
+				result = new ModelAndView("redirect:/welcome/index.do");
+
+			} catch (final Throwable oops) {
+				System.out.println(oops);
+				result = this.createEditModelAndView(createCustomerForm, "customer.commit.error");
+
+			}
+		return result;
+	}
+
+	// Ancillary methods ------------------------------------------------------
+	protected ModelAndView createEditModelAndView(final CreateCustomerForm createCustomerForm) {
 		ModelAndView result;
 
-		result = new ModelAndView("customer/action-2");
+		result = this.createEditModelAndView(createCustomerForm, null);
 
 		return result;
 	}
+
+	protected ModelAndView createEditModelAndView(final CreateCustomerForm createCustomerForm, final String message) {
+		ModelAndView result;
+
+		result = new ModelAndView("customer/create");
+		result.addObject("createCustomerForm", createCustomerForm);
+		result.addObject("requestURI", "customer/create.do");
+		result.addObject("message", message);
+
+		return result;
+	}
+
 }
--- src/main/webapp/views/master-page/header.jsp
+++ src/main/webapp/views/master-page/header.jsp
@@ -30,6 +30,15 @@
 			</li>
 		</security:authorize>
 		
+		<security:authorize access="isAnonymous()">
+			<li><a class="fNiv"><spring:message	code="master.page.register" /></a>
+				<ul>
+					<li class="arrow"></li>
+					<li><a href="customer/create.do"><spring:message code="master.page.customer.register" /></a></li>					
+				</ul>
+			</li>
+		</security:authorize>
+		
 		<security:authorize access="hasRole('CUSTOMER')">
 			<li><a class="fNiv"><spring:message	code="master.page.customer" /></a>
 				<ul>
